1f1808e5941af9e6aa1a1b4c6677038e42cc958f
Nicole Stellatos <stellatosnicole@gmail.com>
2020-08-07 03:11:23 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,43 @@
 class BankAccount
-
+  attr_accessor :balance, :status
+  attr_reader :name
+  def initialize(name, balance=1000, status="open")
+    @name = name
+    @balance = balance
+    @status = status
+  end
+  def deposit(amount)
+    self.balance += amount
+  end
+  def withdrawal(amount)
+    self.balance -= amount
+  end
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+  def valid?
+    self.status == "open" && self.balance > 0 ? true : false
+  end
+  def close_account
+    self.status = "closed"
+end
+  attr_accessor :balance, :status
+  attr_reader :name
+  def initialize(account_name)
+    @name = account_name
+    @balance = 1000
+    @status = "open"
+  end
+  def deposit(money)
+    @balance += money
+  end
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+  def valid?
+    @status == "open" && @balance > 0 ? true : false
+  end
+  def close_account
+    @status = "closed"
+  end
 end
